33019f5d9ff185585e89720773b21c7a41d7ed53
leol1111 <158245605+leol1111@users.noreply.github.com>
2024-04-22 11:26:30 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,33 +2,8 @@
 const nextConfig = {
   output: 'export',
   env: {
-        baseUrl: 'https://leol1111.github.io/faqssearch',
-      },
-  async headers() {
-    return [
-      {
-        source: "/faqssearch/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          {
-            key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
-          }
-        ],
-      },
-    ];
-  },
+        baseUrl: 'https://localhost:3000',
+      }
 };
 
 export default nextConfig;
