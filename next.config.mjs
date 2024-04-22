38dd8ed4fce7764872ee06baeb99ad4b32563f6b
leol1111 <158245605+leol1111@users.noreply.github.com>
2024-04-22 11:43:28 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,25 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: 'Permissions-Policy',
+    value: 'interest-cohort=()'
+  }
+];
+
 const nextConfig = {
   output: 'export',
   env: {
         baseUrl: 'https://localhost:3000',
-      }
+      },
+  async headers() {
+    return [
+      {
+        // Thêm các header an ninh
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
